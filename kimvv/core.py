0e49c36341d44d095a91d0f3a7ed37d91f13b569
Eric Fuemmeler <efuemmel@.umn.edu>
2025-08-07 09:16:38 -0500

    Override call decorator

--- kimvv/core.py
+++ kimvv/core.py
@@ -1,8 +1,10 @@
 import os
 import pathlib
+from typing import Any
 
 import kim_edn
 
+import kimvv 
 
 class KIMVVTestDriver:
     @property
@@ -10,3 +12,56 @@ class KIMVVTestDriver:
         mypath = pathlib.Path(__file__).parent.resolve()
         myname = self.__class__.__name__
         return kim_edn.load(os.path.join(mypath, myname, "kimspec.edn"))
+
+    def _resolve_dependencies(self, **kwargs):
+        # defaults to equilibrium but can be defined within TestDriver for driver specific needs
+        # updates kwargs if needed and returns
+        if isinstance(self, kimvv.EquilibriumCrystalStructure):
+            return kwargs
+        else:
+            print ("Resolving dependencies...")
+            ecs_test = kimvv.EquilibriumCrystalStructure(self._calc)
+            ecs_test(self._get_atoms())
+            # is this fragile?
+            self._update_nominal_parameter_values(ecs_test._get_atoms())
+            return kwargs
+
+# new call decorator
+def override_call_method(cls):
+    def __call__(self, material: Any = None, **kwargs):
+        """
+        Taken from kim-tools with added dependency functionality
+        Main operation of a Test Driver:
+
+            * Run :func:`~KIMTestDriver._setup` (the base class provides a barebones
+              version, derived classes may override)
+            * Call :func:`~KIMTestDriver._calculate` (implemented by each individual
+              Test Driver)
+
+        Args:
+            material:
+                Placeholder object for arguments describing the material to run
+                the Test Driver on
+
+        Returns:
+            The property instances calculated during the current run
+        """
+        # count how many instances we had before we started
+        previous_properties_end = len(self.property_instances)
+
+        os.makedirs("output", exist_ok=True)
+
+        # _setup is likely overridden by an derived class
+        self._setup(material, **kwargs)
+        # resolve dependencies-since input to calculate may depend on output, return kwargs
+        kwargs = self._resolve_dependencies(**kwargs)
+
+        # implemented by each individual Test Driver
+        self._calculate(**kwargs)
+
+        # The current invocation returns a Python list of dictionaries containing all
+        # properties computed during this run
+        return self.property_instances[previous_properties_end:]
+        
+    cls.__call__ = __call__
+    return cls
